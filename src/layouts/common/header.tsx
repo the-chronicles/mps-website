8051bd19a6c1ecdd81cb4b7b5ccba59edacc9e0c
Oluwaseun Akeredolu <oluseungideon@gmail.com>
2024-07-19 13:29:36 +0100

    home

--- src/layouts/common/header.tsx
+++ src/layouts/common/header.tsx
@@ -13,6 +13,7 @@ import Logo from "../../components/logo";
 import NavLink from "../../components/navLink";
 import { HambergerMenu } from "iconsax-react";
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -21,11 +22,13 @@ const Header = () => {
     <Box px={10} pos={"fixed"} width={"100%"} zIndex={99} py={5} bg={"#fff"}>
       <Flex justifyContent={"space-between"} alignItems={"center"}>
         <Box>
-          <Logo />
+          <Link to="/">
+            <Logo />
+          </Link>
         </Box>
 
         <Flex display={{ lg: "flex", md: "flex", sm: "none", base: "none" }}>
-          <NavLink link="/" text="Home" />
+          {/* <NavLink link="/" text="Home" /> */}
           <NavLink link="/about" text="About US" />
           <NavLink link="/partnership" text="Partnership" />
           <NavLink link="/giving" text="Giving" />
