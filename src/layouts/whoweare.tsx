fdeb15e7d929fc191160dc3653b1edba8a30e4ff
Timi-G <56408619+Timi-G@users.noreply.github.com>
2024-07-28 07:16:50 +0100

    Update whoweare.tsx

--- src/layouts/whoweare.tsx
+++ src/layouts/whoweare.tsx
@@ -16,9 +16,9 @@ const Whoweare = () => {
             fontSize={{ lg: 50, md: 40, sm: 20, base: 20 }}
             fontWeight={600}
           >
-            On a Mission to Unleash Potentials. <\br>
+            On a Mission to Unleash Potentials. <br/>
 
-The fastest-growing church in Redemption City <\br>
+The fastest-growing church in Redemption City <br/>
 
 Raising a new generation of young believers who are excited about Christ, have a vision for world transformation, and are committed to sharing the gospel with those who have yet to believe.
           </Text>
