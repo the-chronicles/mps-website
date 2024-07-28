a11f029e3c206100968d5012b43f57e1ae4b5784
Timi-G <56408619+Timi-G@users.noreply.github.com>
2024-07-28 07:46:52 +0100

    Update onlinegiving.tsx

--- src/layouts/onlinegiving.tsx
+++ src/layouts/onlinegiving.tsx
@@ -21,15 +21,15 @@ const Onlinegiving = () => {
             We believe, according to God’s word, that when we give, we invite God’s good measure: pressed down, shaken together, and overflowing with blessings. <br />
           <Text
             mt={5}
-            fontSize={{ lg: 20, md: 20, sm: 10, base: 10 }}
-            fontWeight={300}
+            fontSize={{ lg: 30, md: 30, sm: 10, base: 10 }}
+            fontWeight={600}
           >
             <br/>
             “Give, and it will be given to you. A good measure, pressed down, shaken together and running over, will be poured into your lap…”
             </Text>
             <Text
             mt={5}
-            fontSize={{ lg: 50, md: 40, sm: 20, base: 20 }}
+            fontSize={{ lg: 40, md: 35, sm: 15, base: 15 }}
             fontWeight={600}
           >
               New International Version
